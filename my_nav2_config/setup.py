270c867613f0ff89e90279e257aaa75a0dae9dfa
strix190788 <strix190788@gmail.com>
2026-03-28 16:25:00 +0400

    slam update

--- my_nav2_config/setup.py
+++ my_nav2_config/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'my_nav2_config'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), 
+            glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
